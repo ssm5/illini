fa40085866e20709b38d89be40240ed020899da8
John Qian <johnlongqian@gmail.com>
2017-11-06 14:28:07 -0600

    Added more fields to models, added scraper, added venv

--- app/models.py
+++ app/models.py
@@ -7,8 +7,14 @@ from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class Tag(models.Model):
-    name = models.CharField(_('Name'), max_length=64, unique=True)
-    organizations = models.IntegerField(_('Organization'), default=0)
+    TAG_BREADTHS = (
+        ('B', 'BROAD'),
+        ('S', 'SPECIFIC'),
+        ('N', 'NICHE')
+    )
+    name = models.CharField(_('Tag'), max_length=64, unique=True)
+    breadth = models.CharField(_('Breadth'), max_length=1, choices=TAG_BREADTHS, default='N')
+    # organizationCount = models.IntegerField(_('Organization Count'), default=0)
 
     def __str__(self):
         return self.name
@@ -23,19 +29,25 @@ class Tag(models.Model):
 
 
 class Organization(models.Model):
-    name = models.CharField(_('Name'), max_length=200, unique=True)
-    abbr = models.CharField(_('Abbreviation'), max_length=10, unique=True, default='')
-    found_date = models.DateField(_('Date Founded'))
+    tags = models.ManyToManyField(Tag)
+    name = models.CharField(_('Name'), max_length=64, unique=True)
+    abbr = models.CharField(_('Abbreviation'), max_length=10, unique=True, null=True, blank=True, default='')
+    found_date = models.DateTimeField(_('Date Founded'))
+    end_date = models.DateTimeField(_('Date Ended'), null=True, blank=True)
+    summary = models.TextField(_('Summary'), max_length=256, default='')
     description = models.TextField(_('Description'), default='')
     logo = models.ImageField(_('Logo'), blank=True, null=True)
     members = models.IntegerField(_('Number of Members'), default=0)
-    tags = models.ManyToManyField(Tag)
+    email = models.EmailField(_('Organization Email'), null=True, blank=True)
+    is_public = models.BooleanField(_('Is Public'), default=False)
+    last_modified = models.DateTimeField(_('Last Modified'), null=True, blank=True, auto_now_add=True)
+    is_deleted = models.BooleanField(_('Deleted'), default=False)
 
     def __str__(self):
-        return self.name + " (" + self.abbr + " )"
+        return self.name + (" (" + self.abbr + " )" if self.abbr else "")
 
     def __unicode__(self):
-        return self.name + " (" + self.abbr + " )"
+        return self.name + (" (" + self.abbr + " )" if self.abbr else "")
 
     class Meta:
         verbose_name = _('RSO')
---
+++ app/scraper.py
@@ -0,0 +1,29 @@
+from urllib import request
+# from bs4 import BeautifulSoup
+import json
+import pprint
+import sys
+from app.models import Organization
+
+# if __name__ == "__main__":
+print(sys.path)
+pp = pprint.PrettyPrinter()
+URL_BASE = 'https://illinois.campuslabs.com/engage/api/discovery/organization/bykey/'
+
+with open('app/organizations.json') as file:
+    organizations = json.load(file)
+    for i in range(10, 50):
+        response = request.urlopen(URL_BASE + organizations['value'][i]['WebsiteKey'])
+        org_data = json.loads(response.read().decode())
+        pp.pprint(org_data)
+        org_object = Organization(name=org_data['name'],
+                                  abbr=(org_data['shortName'] if org_data['shortName'] else None),
+                                  found_date=org_data['startDate'],
+                                  summary=org_data['summary'],
+                                  description=org_data['description'] if org_data['description'] else org_data['summary'],
+                                  is_public=True,
+                                  email=org_data['email'],
+                                  last_modified=org_data['modifiedOn'],
+                                  is_deleted=org_data['deleted']
+                                  )
+        org_object.save()
